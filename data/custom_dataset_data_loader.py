70a10fb97aa685c144c842a9ded2517340d400a0
q-zh <41010137+q-zh@users.noreply.github.com>
2021-04-05 23:25:23 +0800

    Update custom_dataset_data_loader.py

--- data/custom_dataset_data_loader.py
+++ data/custom_dataset_data_loader.py
@@ -37,7 +37,32 @@ class CustomDatasetDataLoader(BaseDataLoader):
             if i >= self.opt.max_dataset_size:
                 break
             yield data
+            
+class CustomDatasetDataLoaderTest(BaseDataLoader):
+    def name(self):
+        return 'CustomDatasetDataLoaderTest'
 
+    def initialize(self, opt):
+        BaseDataLoader.initialize(self, opt)
+        self.dataset = CreateDatasetTest(opt)
+        self.dataloader = torch.utils.data.DataLoader(
+            self.dataset,
+            batch_size=opt.batch_size,
+            shuffle=not opt.serial_batches,
+            num_workers=int(opt.nThreads))
+
+    def load_data(self):
+        return self
+
+    def __len__(self):
+        return min(len(self.dataset), self.opt.max_dataset_size)
+
+    def __iter__(self):
+        for i, data in enumerate(self.dataloader):
+            if i >= self.opt.max_dataset_size:
+                break
+            yield data
+            
 def CreateDataset(opt):
     dataset = None
     dataset = RemovalDataset()
@@ -52,6 +77,11 @@ def CreateDatasetTest(opt):
     dataset.initialize(opt)
     return dataset
 
+def CreateDataLoaderTest(opt):
+    data_loader = CustomDatasetDataLoaderTest()
+    print(data_loader.name())
+    data_loader.initialize(opt)
+    return data_loader
 
 def CreateDataLoader(opt):
     data_loader = CustomDatasetDataLoader()
